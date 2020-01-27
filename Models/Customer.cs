f1d53244a5f8c9a70081c9d872b4dfd20facd607
Ryan Emery <re259315@ohio.edu>
2020-01-27 11:41:15 -0500

    added customer model

---
+++ Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace re259315_MIS4200.Models
+{
+    public class Customer
+    {
+        public int customerID { get; set; }
+        public string customerFirstName { get; set; }
+        public string customerLastName { get; set; }
+        public string email { get; set; }
+        public int phone { get; set; }
+        public DateTime customerSince { get; set; }
+    }
+}
